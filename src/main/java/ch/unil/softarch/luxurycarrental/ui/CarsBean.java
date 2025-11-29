648e4e43378135d9dd72050307d05ac8ad7c49a4
Hang Yang <yhangg666@gmail.com>
2025-11-29 13:36:05 +0100

    Main functional methods exploration completed

---
+++ src/main/java/ch/unil/softarch/luxurycarrental/ui/CarsBean.java
@@ -0,0 +1,35 @@
+package ch.unil.softarch.luxurycarrental.ui;
+
+import ch.unil.softarch.luxurycarrental.client.CarClient;
+import ch.unil.softarch.luxurycarrental.domain.entities.Car;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+@Named("carsBean")
+@ViewScoped
+public class CarsBean implements Serializable {
+
+    @Inject
+    private CarClient carClient;
+
+    private List<Car> carList;
+
+    @PostConstruct
+    public void init() {
+        carList = carClient.getAllCars();  // REST 获取全部车辆
+    }
+
+    public String goToDetails(UUID id) {
+        return "cars_details.xhtml?faces-redirect=true&carId=" + id;
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+}
